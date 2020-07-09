6432f25c55a76213390ab9bcd2cb20da97a0a81e
grt812 <31299429+grt812@users.noreply.github.com>
2020-07-09 15:01:26 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -12,16 +12,39 @@ function httpGetAsync(theUrl, callback){
 }
 
 $(function(){
-
+  let nameArray = ["content"];
   let repoResult = httpGetAsync("https://api.github.com/users/grt812/repos", function(result){
     console.log(typeof result);
     result.forEach(function(e){
-      $("#repo-list").append(`
+      if(e.homepage != null && e.homepage !== ""){
+        console.log("URL: '"+e.homepage+"'");
+        $("#repo-list").append(`
         <a class="list" href="${e.homepage}" target="_blank"><span>${e.name}</span><button data-href="${e.html_url}"><img src="GitHub-Mark-32px.png" alt="GitHub"></button></a>`);
+        nameArray.push(e.name);
+      } else {
+        // console.log(e.homepage);
+      }
     });
     $("[data-href]").click(function(){
       window.open($(this).attr("data-href"));
     });
+    for(let i = 0; i < 100; i++){
+      let chosenRepo = nameArray[parseInt(Math.random() * nameArray.length)];
+      $("#relative-container").append("<span class=\"content\">"+chosenRepo+"</span>");
+    }
+    $(".content").each(function(){
+        // $(this).css("top",);
+        // $(this).css("left",Math.random()*($(window).width() - 30));
+        let xPos = $(window).width()*Math.random()-$(window).width()/2;
+        let yPos = $(window).height()*Math.random()-$(window).height()/2;
+        $(this).css("transform", "translate("+xPos+"px,"+yPos+"px) rotate("+Math.random()*360+"deg) scale("+(Math.random()*5)+","+(Math.random()*5)+")");
+    });
   });
+
+  // setInterval(function(){
+  //   $(".content").each(function(){
+  //
+  //   });
+  // },50);
 
 });
